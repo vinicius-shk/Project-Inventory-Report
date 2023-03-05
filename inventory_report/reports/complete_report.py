49f9123f1f5fdda6c07f00f582d3c142a4210ef3
NoteVini <vinicius.shk@gmail.com>
2023-03-05 12:52:19 -0300

    feat: Implements a complete report method

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from simple_report import SimpleReport
+from typing import List, Dict
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(my_list: List[Dict[str, str]]):
+        superclass_ret = super(
+            CompleteReport, CompleteReport
+            ).generate(my_list)
+        ordered_comp_prod_counter = dict()
+        comp_quant = ''
+        for item in my_list:
+            if item["nome_da_empresa"] not in ordered_comp_prod_counter:
+                ordered_comp_prod_counter[item["nome_da_empresa"]] = 1
+            ordered_comp_prod_counter[item["nome_da_empresa"]] += 1
+        items_list = ordered_comp_prod_counter.items()
+        for key, value in items_list:
+            comp_quant += f'{key}: {value}\n'
+        return f'''{superclass_ret}
+Produtos estocados por empresa:
+{comp_quant}
+        '''
